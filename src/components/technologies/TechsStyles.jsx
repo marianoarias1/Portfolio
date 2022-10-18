3a0aa0d45769170575a50ba91e1d64ba367c08ca
Mariano Arias <marianoarias123@hotmail.com>
2022-10-18 12:30:41 -0300

    responsive

--- src/components/technologies/TechsStyles.jsx
+++ src/components/technologies/TechsStyles.jsx
@@ -49,6 +49,13 @@ export const TitleContainer= styled.div`
 
 export const AllContainer=styled.div`
 
+display: flex;
+justify-content: center;
+align-items: center;
+width: 100%;
+height: 100%;
+flex-direction: column;
+
 @media (max-width: 391px) {
         width: 80%;
         height: 80%;
